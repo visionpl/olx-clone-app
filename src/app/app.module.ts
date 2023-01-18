2f5b70a48092a2f1e3560cdcf98c0dcdd68f599b
Patryk Wisniewicz <wisniewicz.patryk@gmail.com>
2023-01-18 21:45:11 +0100

    create login-page component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,9 +3,10 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
+import { LoginPageComponent } from './login-page/login-page.component';
 
 @NgModule({
-  declarations: [AppComponent, HeaderComponent],
+  declarations: [AppComponent, HeaderComponent, LoginPageComponent],
   imports: [BrowserModule],
   providers: [],
   bootstrap: [AppComponent],
