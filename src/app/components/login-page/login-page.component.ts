6646420ccba28e9b89abb67c2a881cd9d1181338
Patryk Wisniewicz <wisniewicz.patryk@gmail.com>
2023-02-09 18:42:19 +0100

    clearing code

--- src/app/components/login-page/login-page.component.ts
+++ src/app/components/login-page/login-page.component.ts
@@ -11,7 +11,6 @@ export class LoginPageComponent {
   constructor(private router: Router) {}
 
   onLogin(value: string) {
-    // dodac w local storage
     localStorage.setItem('emailInput', value);
     this.router.navigate(['/']);
   }
