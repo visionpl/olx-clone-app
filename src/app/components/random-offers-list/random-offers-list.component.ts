b06c217ef4058aa4f733ff6dd7770e79aca0bc32
Patryk Wisniewicz <wisniewicz.patryk@gmail.com>
2023-02-28 22:27:53 +0100

    feature(randomoffers): add random offers component

---
+++ src/app/components/random-offers-list/random-offers-list.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-random-offers-list',
+  templateUrl: './random-offers-list.component.html',
+  styleUrls: ['./random-offers-list.component.css']
+})
+export class RandomOffersListComponent {
+
+}
