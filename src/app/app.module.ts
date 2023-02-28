6a69f05a77219cfcf16e058085e3c367bc269253
Patryk Wisniewicz <wisniewicz.patryk@gmail.com>
2023-02-28 21:57:30 +0100

    feat(search): add search component UI

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,9 +7,10 @@ import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
 import { LoginPageComponent } from './components/login-page/login-page.component';
 import { MainComponent } from './components/main/main.component';
+import { SearchComponent } from './components/search/search.component';
 
 @NgModule({
-  declarations: [AppComponent, HeaderComponent, LoginPageComponent, MainComponent],
+  declarations: [AppComponent, HeaderComponent, LoginPageComponent, MainComponent, SearchComponent],
   imports: [BrowserModule, AppRoutingModule, FormsModule],
   providers: [],
   bootstrap: [AppComponent],
