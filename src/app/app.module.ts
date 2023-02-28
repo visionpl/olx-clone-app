7768296c4086a00d4d3197b35f6c29a0a1a41ce9
Patryk Wisniewicz <wisniewicz.patryk@gmail.com>
2023-02-28 22:20:45 +0100

    feat(categories): create categories component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,9 +8,12 @@ import { HeaderComponent } from './components/header/header.component';
 import { LoginPageComponent } from './components/login-page/login-page.component';
 import { MainComponent } from './components/main/main.component';
 import { SearchComponent } from './components/search/search.component';
+import { CategoriesComponent } from './components/categories/categories.component';
+import { SingleCategoryComponent } from './components/categories/single-category/single-category.component';
+import { RandomOffersListComponent } from './components/random-offers-list/random-offers-list.component';
 
 @NgModule({
-  declarations: [AppComponent, HeaderComponent, LoginPageComponent, MainComponent, SearchComponent],
+  declarations: [AppComponent, HeaderComponent, LoginPageComponent, MainComponent, SearchComponent, CategoriesComponent, SingleCategoryComponent, RandomOffersListComponent],
   imports: [BrowserModule, AppRoutingModule, FormsModule],
   providers: [],
   bootstrap: [AppComponent],
