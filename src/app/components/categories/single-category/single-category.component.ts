0e4bee1653947feb534a8167bf83b8d9683bbb95
Patryk Wisniewicz <wisniewicz.patryk@gmail.com>
2023-03-01 21:43:43 +0100

    feat(categories): add category box and simple data

--- src/app/components/categories/single-category/single-category.component.ts
+++ src/app/components/categories/single-category/single-category.component.ts
@@ -1,10 +1,10 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-single-category',
   templateUrl: './single-category.component.html',
-  styleUrls: ['./single-category.component.css']
+  styleUrls: ['./single-category.component.css'],
 })
 export class SingleCategoryComponent {
-
+  @Input() categories = [];
 }
