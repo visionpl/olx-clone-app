93ffad9101401b9e89ad2f44905014757896eb29
Patryk Wisniewicz <wisniewicz.patryk@gmail.com>
2023-03-02 17:45:56 +0100

    feat(randomoffers,categories): add helpers

--- src/app/components/categories/categories.component.ts
+++ src/app/components/categories/categories.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { categoriesList } from 'src/app/helpers/helper';
 
 @Component({
   selector: 'app-categories',
@@ -6,78 +7,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./categories.component.css'],
 })
 export class CategoriesComponent {
-  category = [
-    {
-      id: 1,
-      name: 'Kategoria 1',
-      image:
-        'https://images.pexels.com/photos/6605302/pexels-photo-6605302.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
-    },
-    {
-      id: 2,
-      name: 'Kategoria 2',
-      image:
-        'https://images.pexels.com/photos/13938459/pexels-photo-13938459.jpeg?auto=compress&cs=tinysrgb&w=1600&lazy=load',
-    },
-    {
-      id: 3,
-      name: 'Kategoria 3',
-      image:
-        'https://images.pexels.com/photos/5859489/pexels-photo-5859489.jpeg?auto=compress&cs=tinysrgb&w=1600&lazy=load',
-    },
-    {
-      id: 4,
-      name: 'Kategoria 4',
-      image:
-        'https://images.pexels.com/photos/13147620/pexels-photo-13147620.jpeg?auto=compress&cs=tinysrgb&w=1600&lazy=load',
-    },
-    {
-      id: 5,
-      name: 'Kategoria 5',
-      image:
-        'https://images.pexels.com/photos/7966000/pexels-photo-7966000.jpeg?auto=compress&cs=tinysrgb&w=1600&lazy=load',
-    },
-    {
-      id: 6,
-      name: 'Kategoria 6',
-      image:
-        'https://images.pexels.com/photos/15737949/pexels-photo-15737949.jpeg?auto=compress&cs=tinysrgb&w=1600&lazy=load',
-    },
-    {
-      id: 7,
-      name: 'Kategoria 7',
-      image:
-        'https://images.pexels.com/photos/13575099/pexels-photo-13575099.jpeg?auto=compress&cs=tinysrgb&w=1600&lazy=load',
-    },
-    {
-      id: 8,
-      name: 'Kategoria 8',
-      image:
-        'https://images.pexels.com/photos/15047680/pexels-photo-15047680.jpeg?auto=compress&cs=tinysrgb&w=1600&lazy=load',
-    },
-    {
-      id: 9,
-      name: 'Kategoria 9',
-      image:
-        'https://images.pexels.com/photos/6580699/pexels-photo-6580699.jpeg?auto=compress&cs=tinysrgb&w=1600&lazy=load',
-    },
-    {
-      id: 10,
-      name: 'Kategoria 10',
-      image:
-        'https://images.pexels.com/photos/15212791/pexels-photo-15212791.jpeg?auto=compress&cs=tinysrgb&w=1600&lazy=load',
-    },
-    {
-      id: 11,
-      name: 'Kategoria 11',
-      image:
-        'https://images.pexels.com/photos/2718416/pexels-photo-2718416.jpeg?auto=compress&cs=tinysrgb&w=1600&lazy=load',
-    },
-    {
-      id: 12,
-      name: 'Kategoria 12',
-      image:
-        'https://images.pexels.com/photos/8084774/pexels-photo-8084774.jpeg?auto=compress&cs=tinysrgb&w=1600&lazy=load',
-    },
-  ];
+  category = categoriesList;
 }
---
+++ src/app/helpers/helper.ts
@@ -0,0 +1,141 @@
+export const categoriesList = [
+  {
+    id: 1,
+    name: 'Kategoria 1',
+    image:
+      'https://images.pexels.com/photos/6605302/pexels-photo-6605302.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
+  },
+  {
+    id: 2,
+    name: 'Kategoria 2',
+    image:
+      'https://images.pexels.com/photos/13938459/pexels-photo-13938459.jpeg?auto=compress&cs=tinysrgb&w=1600&lazy=load',
+  },
+  {
+    id: 3,
+    name: 'Kategoria 3',
+    image:
+      'https://images.pexels.com/photos/5859489/pexels-photo-5859489.jpeg?auto=compress&cs=tinysrgb&w=1600&lazy=load',
+  },
+  {
+    id: 4,
+    name: 'Kategoria 4',
+    image:
+      'https://images.pexels.com/photos/13147620/pexels-photo-13147620.jpeg?auto=compress&cs=tinysrgb&w=1600&lazy=load',
+  },
+  {
+    id: 5,
+    name: 'Kategoria 5',
+    image:
+      'https://images.pexels.com/photos/7966000/pexels-photo-7966000.jpeg?auto=compress&cs=tinysrgb&w=1600&lazy=load',
+  },
+  {
+    id: 6,
+    name: 'Kategoria 6',
+    image:
+      'https://images.pexels.com/photos/15737949/pexels-photo-15737949.jpeg?auto=compress&cs=tinysrgb&w=1600&lazy=load',
+  },
+  {
+    id: 7,
+    name: 'Kategoria 7',
+    image:
+      'https://images.pexels.com/photos/13575099/pexels-photo-13575099.jpeg?auto=compress&cs=tinysrgb&w=1600&lazy=load',
+  },
+  {
+    id: 8,
+    name: 'Kategoria 8',
+    image:
+      'https://images.pexels.com/photos/15047680/pexels-photo-15047680.jpeg?auto=compress&cs=tinysrgb&w=1600&lazy=load',
+  },
+  {
+    id: 9,
+    name: 'Kategoria 9',
+    image:
+      'https://images.pexels.com/photos/6580699/pexels-photo-6580699.jpeg?auto=compress&cs=tinysrgb&w=1600&lazy=load',
+  },
+  {
+    id: 10,
+    name: 'Kategoria 10',
+    image:
+      'https://images.pexels.com/photos/15212791/pexels-photo-15212791.jpeg?auto=compress&cs=tinysrgb&w=1600&lazy=load',
+  },
+  {
+    id: 11,
+    name: 'Kategoria 11',
+    image:
+      'https://images.pexels.com/photos/2718416/pexels-photo-2718416.jpeg?auto=compress&cs=tinysrgb&w=1600&lazy=load',
+  },
+  {
+    id: 12,
+    name: 'Kategoria 12',
+    image:
+      'https://images.pexels.com/photos/8084774/pexels-photo-8084774.jpeg?auto=compress&cs=tinysrgb&w=1600&lazy=load',
+  },
+];
+
+export const randomOffersList = [
+  {
+    id: 1,
+    name: 'Lorem ipsum dolor sit amet',
+    district: 'Wrzeszcz',
+    price: 200,
+    image:
+      'https://images.pexels.com/photos/3232535/pexels-photo-3232535.jpeg?auto=compress&cs=tinysrgb&w=1600',
+  },
+  {
+    id: 2,
+    name: 'Donec hendrerit erat justo',
+    district: 'Żabianka',
+    price: 599,
+    image:
+      'https://images.pexels.com/photos/100582/pexels-photo-100582.jpeg?auto=compress&cs=tinysrgb&w=1600',
+  },
+  {
+    id: 3,
+    name: 'Lorem ipsum dolor sit amet',
+    district: 'Wrzeszcz',
+    price: 254,
+    image:
+      'https://images.pexels.com/photos/3232535/pexels-photo-3232535.jpeg?auto=compress&cs=tinysrgb&w=1600',
+  },
+  {
+    id: 4,
+    name: 'Donec hendrerit erat justo',
+    district: 'Żabianka',
+    price: 1599,
+    image:
+      'https://images.pexels.com/photos/100582/pexels-photo-100582.jpeg?auto=compress&cs=tinysrgb&w=1600',
+  },
+  {
+    id: 5,
+    name: 'Lorem ipsum dolor sit amet',
+    district: 'Wrzeszcz',
+    price: 200,
+    image:
+      'https://images.pexels.com/photos/3232535/pexels-photo-3232535.jpeg?auto=compress&cs=tinysrgb&w=1600',
+  },
+  {
+    id: 6,
+    name: 'Donec hendrerit erat justo',
+    district: 'Żabianka',
+    price: 599,
+    image:
+      'https://images.pexels.com/photos/100582/pexels-photo-100582.jpeg?auto=compress&cs=tinysrgb&w=1600',
+  },
+  {
+    id: 7,
+    name: 'Lorem ipsum dolor sit amet',
+    district: 'Wrzeszcz',
+    price: 254,
+    image:
+      'https://images.pexels.com/photos/3232535/pexels-photo-3232535.jpeg?auto=compress&cs=tinysrgb&w=1600',
+  },
+  {
+    id: 8,
+    name: 'Donec hendrerit erat justo',
+    district: 'Żabianka',
+    price: 1599,
+    image:
+      'https://images.pexels.com/photos/100582/pexels-photo-100582.jpeg?auto=compress&cs=tinysrgb&w=1600',
+  },
+];
