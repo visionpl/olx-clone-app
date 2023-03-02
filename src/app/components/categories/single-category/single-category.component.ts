292e5eb08f5d780854b5252ff6ad571116a874de
Patryk Wisniewicz <wisniewicz.patryk@gmail.com>
2023-03-02 19:06:26 +0100

    refactor(categories): add single-category interfac

--- src/app/components/categories/single-category/single-category.component.ts
+++ src/app/components/categories/single-category/single-category.component.ts
@@ -1,4 +1,5 @@
 import { Component, Input } from '@angular/core';
+import { CategoryItem } from 'src/app/interfaces/CategoryItem';
 
 @Component({
   selector: 'app-single-category',
@@ -6,5 +7,5 @@ import { Component, Input } from '@angular/core';
   styleUrls: ['./single-category.component.css'],
 })
 export class SingleCategoryComponent {
-  @Input() categoryItem = { name: '', image: '' };
+  @Input() categoryItem: CategoryItem;
 }
