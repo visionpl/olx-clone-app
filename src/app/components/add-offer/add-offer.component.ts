6dc7bc4f76e3c04e47c92be5dcd2fca0912a355d
Patryk Wisniewicz <wisniewicz.patryk@gmail.com>
2023-03-04 13:56:50 +0100

    feat(add-offer): bind data from helpers to form

--- src/app/components/add-offer/add-offer.component.ts
+++ src/app/components/add-offer/add-offer.component.ts
@@ -1,10 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { categoriesList, districtList } from 'src/app/helpers/helper';
 
 @Component({
   selector: 'app-add-offer',
   templateUrl: './add-offer.component.html',
-  styleUrls: ['./add-offer.component.css']
+  styleUrls: ['./add-offer.component.css'],
 })
 export class AddOfferComponent {
+  @Input() categoryList = categoriesList;
+  selectedCategoryValue = null;
 
+  emailLogin: string = localStorage.getItem('emailInput');
+
+  districtList = districtList;
+  selectedDistrictValue = null;
 }
