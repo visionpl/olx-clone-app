5c0a5c640b20e87e90e1416d55609f31bbfd9d1e
Patryk Wisniewicz <wisniewicz.patryk@gmail.com>
2023-03-31 16:20:38 +0200

    feat(offers-item,offers-list): update styles

--- src/app/components/search-result/search-offers-list/search-offer-item/search-offer-item.component.ts
+++ src/app/components/search-result/search-offers-list/search-offer-item/search-offer-item.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
+import { faHeart } from '@fortawesome/free-regular-svg-icons';
 
 @Component({
   selector: 'app-search-offer-item',
   templateUrl: './search-offer-item.component.html',
-  styleUrls: ['./search-offer-item.component.css']
+  styleUrls: ['./search-offer-item.component.css'],
 })
 export class SearchOfferItemComponent {
-
+  faHeart = faHeart;
 }
