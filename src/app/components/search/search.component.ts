2d98194856287f6d6b495f3285c88f7734e4e9dd
Patryk Wisniewicz <wisniewicz.patryk@gmail.com>
2023-04-01 18:03:43 +0200

    feat(search): add form validation

--- src/app/components/search/search.component.ts
+++ src/app/components/search/search.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
 import { districtList } from 'src/app/helpers/helper';
 import { SearchService } from 'src/app/services/search.service';
@@ -11,18 +12,27 @@ import { SearchService } from 'src/app/services/search.service';
 export class SearchComponent {
   constructor(private router: Router, private searchService: SearchService) {}
   search: string;
-
   districts = districtList;
 
+  searchForm = new FormGroup({
+    search: new FormControl('', [Validators.required]),
+  });
+
   onSubmitSearch(value: string) {
-    this.searchService.search(this.search);
-    const currentUrl = this.router.url;
-    if (currentUrl === '/search') {
-      this.router.navigateByUrl('/', { skipLocationChange: true }).then(() => {
+    if (this.searchForm.valid) {
+      this.searchService.search(this.search);
+      const currentUrl = this.router.url;
+      if (currentUrl === '/search') {
+        this.router
+          .navigateByUrl('/', { skipLocationChange: true })
+          .then(() => {
+            this.router.navigate(['/search']);
+          });
+      } else {
         this.router.navigate(['/search']);
-      });
+      }
     } else {
-      this.router.navigate(['/search']);
+      this.searchForm.markAllAsTouched();
     }
   }
 }
