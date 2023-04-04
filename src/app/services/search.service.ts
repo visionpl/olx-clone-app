1a86abd12595f2eb7e745dc26e11b37f7eba4879
Patryk Wisniewicz <wisniewicz.patryk@gmail.com>
2023-04-04 21:13:14 +0200

    Revert "refactor(search): change variable names in service"
    This reverts commit 855e37564a9096e633f97b48f9c23ceb7d06288e.

--- src/app/services/search.service.ts
+++ src/app/services/search.service.ts
@@ -10,9 +10,9 @@ export class SearchService {
   results: OfferItem[];
 
   search(value: string) {
-    const offersList = JSON.parse(localStorage.getItem('offers'));
-    this.results = offersList.filter((offer: OfferItem) =>
-      offer.offerName.toLowerCase().includes(value.toLowerCase())
+    const data = JSON.parse(localStorage.getItem('offers'));
+    this.results = data.filter((item) =>
+      item.offerName.toLowerCase().includes(value.toLowerCase())
     );
   }
 
