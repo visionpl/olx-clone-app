2c01f34dcfb9a13711c8797641204112d0820d02
Patryk Wisniewicz <wisniewicz.patryk@gmail.com>
2023-04-16 22:09:30 +0200

    feat(offer): add logic to display offer descript

--- src/app/components/offer/offer-view/offer-view.component.ts
+++ src/app/components/offer/offer-view/offer-view.component.ts
@@ -1,10 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { OfferItem } from 'src/app/interfaces/OfferItem';
 
 @Component({
   selector: 'app-offer-view',
   templateUrl: './offer-view.component.html',
-  styleUrls: ['./offer-view.component.css']
+  styleUrls: ['./offer-view.component.css'],
 })
-export class OfferViewComponent {
+export class OfferViewComponent implements OnInit {
+  offer: OfferItem;
 
+  @Input() idOffer: number;
+
+  ngOnInit() {
+    const offersString = localStorage.getItem('offers');
+    const offers = JSON.parse(offersString);
+    this.offer = offers.find((offer: OfferItem) => offer.id == this.idOffer);
+  }
 }
