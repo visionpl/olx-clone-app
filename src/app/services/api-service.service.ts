a1535fdd9447d6303a00ed133704b48d0ea42e50
Patryk Wisniewicz <wisniewicz.patryk@gmail.com>
2023-05-06 11:33:22 +0200

    feat(api-service): handle signin endpint

--- src/app/services/api-service.service.ts
+++ src/app/services/api-service.service.ts
@@ -1,9 +1,18 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { environment } from '../environment';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class ApiServiceService {
+  private apiUrl = environment.apiEndpoint;
 
-  constructor() { }
+  constructor(private http: HttpClient) {}
+
+  signIn(email: string, password: string) {
+    const url = `${this.apiUrl}/signin`;
+    const body = { email, password };
+    return this.http.post(url, body);
+  }
 }
