ed203925291a15331361caf965e6c1268a97d35c
Patryk Wisniewicz <wisniewicz.patryk@gmail.com>
2023-05-06 12:58:19 +0200

    feat(login-page): add logic to form

--- src/app/components/login-page/login-page.component.ts
+++ src/app/components/login-page/login-page.component.ts
@@ -1,5 +1,7 @@
-import { Component, Output, EventEmitter } from '@angular/core';
+import { Component } from '@angular/core';
 import { Router } from '@angular/router';
+import { ApiService } from 'src/app/services/api.service';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-login-page',
@@ -8,10 +10,36 @@ import { Router } from '@angular/router';
   providers: [],
 })
 export class LoginPageComponent {
-  constructor(private router: Router) {}
+  constructor(private router: Router, private api: ApiService) {}
 
-  onLogin(value: string) {
-    localStorage.setItem('emailInput', value);
-    this.router.navigate(['/']);
+  signInForm = new FormGroup(
+    {
+      email: new FormControl('test@test.com', [
+        Validators.required,
+        Validators.pattern('^[a-z0-9._%+-]+@[a-z0-9.-]+\\.[a-z]{2,4}$'),
+      ]),
+      password: new FormControl('123', [
+        Validators.required,
+        Validators.minLength(3),
+      ]),
+    },
+    { updateOn: 'blur' }
+  );
+
+  onLogin(credentials: any) {
+    if (this.signInForm.valid) {
+      this.api.signIn(credentials.email, credentials.password).subscribe(
+        (response) => {
+          console.log('response', response);
+          // api
+        },
+        (error) => {
+          console.error('error', error);
+          this.signInForm.reset();
+          // api error
+        }
+      );
+    }
+    this.signInForm.markAllAsTouched();
   }
 }
